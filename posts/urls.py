24661d3743794ec1a6a837ab2da2ab55505a5fbd
Mqasim94 <mqasimbhati94@gmail.com>
2024-01-11 17:24:21 +0500

    Qasim changeing

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
-from .views import *
 
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import *
 
 
 urlpatterns = [
@@ -9,3 +12,7 @@ urlpatterns = [
 path('profile/', profile_Create.as_view(), name='profile'),
 
 ]
+        
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
