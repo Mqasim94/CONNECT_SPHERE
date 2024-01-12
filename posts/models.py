9c99157e340d7896291edc423f1e3ef96542b816
Najeebo <najeebtareen00@gmail.com>
2024-01-12 14:32:42 +0500

    Created post model which need urls views templates

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from users.models import User
+from django.utils import timezone
 
 # Create your models here.
 class Profile(models.Model):
@@ -12,3 +13,15 @@ class Profile(models.Model):
     def __str__(self):
         return self.city
     
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    shared_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(default=timezone.now)
+    shared_on = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(upload_to='post_pic', blank=True, null=True)
+    
+    
+    def __str__(self):
+        return  f'{self.shared_user.username} post {self.title}'
