317ab5eafbf3949702bdde324363644c4e1346ed
Najeebo <najeebtareen00@gmail.com>
2024-01-21 19:13:39 +0500

    fixed form errors in signup and signin

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth .forms import UserCreationForm
+from django.core import validators
 from .models import User
 from django import forms
 
@@ -6,8 +7,39 @@ class Register_Form(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+       
+
+      
+
+    def __init__(self, *args, **kwargs):
+        super(Register_Form, self).__init__(*args, **kwargs)
+        
+       
+        self.fields['username'].error_messages = {
+            'required': 'This field is required.',
+            'unique': 'This username is already taken. Please choose a different one.',
+            'invalid': 'Enter a valid username.'
+        }
+
+        self.fields['email'].error_messages = {
+            'required': 'This field is required.',
+            'unique': 'This email address is already registered. Please use a different one.',
+            'invalid': 'Enter a valid email address.',
+            'invalid_at_sign': 'Email must contain the "@" sign.',
+        }
+
+        self.fields['password1'].error_messages = {
+            'required': 'This field is required.',
+            'min_length': 'Password must be at least 8 characters long.',
+            'password_mismatch': 'The two password fields didnt match.'
+        }
+
+        self.fields['password2'].error_messages = {
+            'required': 'This field is required.',
+            'password_mismatch': 'The two password fields didnt match.'
+        }
 
 
 class Signin_Form(forms.Form):
-    username = forms.CharField(max_length= 128)
+    username = forms.CharField( max_length=200)
     password = forms.CharField(widget=forms.PasswordInput)
--- users/views.py
+++ users/views.py
@@ -6,26 +6,8 @@ from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from .forms import Register_Form, Signin_Form
-# Create your views here.
 
-# class Register(CreateView):
-#     model = User
-#     template_name = 'users/register.html'
-#     form = UserCreationForm
-#     success_url = '/users/signin/'
 
-# def register_page(request):
-#     form = Register_Form()
-#     if request.method == 'POST':
-#         form = Register_Form(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('signin')
-        
-#     else:
-#         form = Register_Form()
-
-#     return render(request, 'users/register.html', {'form':form})
 
 class Register(CreateView):
     model = User
@@ -52,68 +34,15 @@ class Signin(View):
                 username=form.cleaned_data['username'],
                 password=form.cleaned_data['password'],
             )
-            if user is not None:
+            if user:
                 login(request, user)
                 return redirect('/posts/List_Post/')
-        message = 'Login failed!'
+            else:
+                return HttpResponse('wrong Credentials')
         
-#     from django.contrib.auth import authenticate, login
-# from django.shortcuts import render, redirect
-
-# class Signin(View):
-#     template_name = 'users/signin.html'
-#     form_class = Signin_Form
-#     success_url = '/posts/post_list/'
-
-#     def get(self, request):
-#         form = self.form_class()
-#         message = 'nooo'
-#         return render(request, self.template_name, context={'form': form, 'message': message})
-
-#     def post(self, request):
-#         form = self.form_class(request.POST)
-#         message = ''
-#         if form.is_valid():
-#             user = authenticate(
-#                 username=form.cleaned_data['username'],
-#                 password=form.cleaned_data['password'],
-#             )
-#             if user is not None:
-#                 login(request, user)
-#                 return redirect('/posts/List_Post/')
-#         else:
-#             # Form is not valid, handle errors
-#             errors = form.errors.as_data()
-#             error_messages = []
-#             for field, error in errors.items():
-#                 error_messages.append(f"{field}: {error[0].message}")
-#             message = '\n'.join(error_messages)
-
-#         return render(request, self.template_name, context={'form': form, 'message': message})
+        
 
 
-
-
-class LogoutView(LogoutView):
-
-    success_url= '/users/home.html'
-
-
-
-# def signin (request):
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-#         user = authenticate (request, username=username, password=password)
-
-#         if user is not None:
-#             login(request, user)
-#             return HttpResponse ("loged In")        
-#         else: 
-#             messages.error(request, "Try Again!, 'Username' and 'Password' is incorrect")
-
-#     context = {}
-#     return render(request, 'users/signin.html', context)
 
 def home(request):
 
